ee86f7a0a758ed6c8e6b392755daf65a6a5d5214
jon199835-crypto <jon199835@gmail.com>
2025-10-01 23:02:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 // форвард POST в Google Apps Script
 app.post("/", async (req, res) => {
   try {
-    const resp = await fetch("https://script.google.com/macros/s/AKfycbxA4axyfqq03_nH8QuVIJFVnsLtBDqcpMJOPY5vYvoaa3xvAEj6Z_pwmOEQfyhjjpVj/exec", {
+    const resp = await fetch("https://script.google.com/macros/s/AKfycbzSluk9nS14bWNwm7KiiKNdUoBsXF7RI5wGcdDD4JPY07bhze3yciKwrFJ012ef_bWw/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(req.body)
