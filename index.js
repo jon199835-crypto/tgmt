8c78f2fec3945f3260bd61f82a3d1e5369f156de
jon199835-crypto <jon199835@gmail.com>
2025-10-13 22:49:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 // === CONFIG ===
-const GAS_URL = "https://script.google.com/macros/s/AKfycbw8LPpWNvOIJ49cX578AuSfsFrgBINSpHvXumR58ctnvxr-3wt-QLroeLYapWTkoJLViA/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwCnkbapwxgH1UU3Sqf4fIhLGj0KhuFSPN6t1kZjT2wM09Vlr9ZSDBvrdAtImTPQ590fA/exec";
 
 // === ПРОКСИ ДЛЯ TELEGRAM ===
 app.post("/", async (req, res) => {
